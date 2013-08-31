04c08a28623ad560689f946250ceb09f9bbd21e4
Aaron O'Mullan <aaron.omullan@friendco.de>
2013-08-31 03:12:10 -0700

    Remove shoe & mux-demux, replace with socket.io-stream

---
+++ tests/shell_io.js
@@ -0,0 +1,47 @@
+var es = require('event-stream');
+var io = require('socket.io-client');
+var ss = require('socket.io-stream');
+
+
+// Setup
+var socket = io.connect('http://localhost:8000/stream/shells');
+var stream = ss.createStream();
+
+
+// Shell args
+var args = {
+    shellId: process.argv[2],
+    opts: {
+        rows: process.stdout.rows,
+        columns: process.stdout.columns
+    }
+};
+
+// Send stream to server
+ss(socket).emit('shell.open', stream, args);
+
+var state = { meta: false };
+var keyboard = es.through(function (buf) {
+    if (buf.length === 1 && buf[0] === 1) return state.meta = true;
+
+    if (state.meta && buf[0] === 'd'.charCodeAt(0)) {
+        process.exit();
+    }
+    else this.queue(buf);
+    state.meta = false;
+});
+
+// Piping
+keyboard.pipe(stream).pipe(process.stdout);
+
+process.stdin.setRawMode(true);
+process.stdin.pipe(keyboard);
+
+process.on('exit', function () {
+    process.stdin.setRawMode(false);
+    console.log('\n[shux exited]');
+});
+
+stream.on('end', function() {
+    process.exit();
+});
