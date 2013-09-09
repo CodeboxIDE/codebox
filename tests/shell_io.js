7c7d81e591158e31ad5f81ba0fb3ea804cf6d65e
Samy <samypesse@gmail.com>
2013-09-09 09:02:34 +0000

    test commiting from friendcode

--- tests/shell_io.js
+++ tests/shell_io.js
@@ -8,7 +8,7 @@ var socket = io.connect('http://localhost:8000/stream/shells');
 var stream = ss.createStream();
 
 
-// Shell args
+// Shell args (test)
 var args = {
     shellId: process.argv[2],
     opts: {
