a2bed232988b39ba063aa035046a6044e17f3d8b
Aaron O'Mullan <aaron.omullan@friendco.de>
2013-12-02 02:33:02 -0800

    Add theme support to terminal addon

--- src/addons/cb.terminal/client.js
+++ src/addons/cb.terminal/client.js
@@ -10,7 +10,8 @@ define([
         'namespace': "terminal",
         'title': "Terminal",
         'defaults': {
-            'font': "monospace"
+            'font': "monospace",
+            'theme': 'monokai_soda'
         },
         'fields': {
             'font': {
@@ -22,6 +23,38 @@ define([
                     'Courier New': "Courier New",
                     "'MS Sans Serif', Geneva, sans-serif;": "MS Sans Serif",
                     "'Lucida Sans Unicode', 'Lucida Grande', sans-serif": "Lucida Sans Unicode"
+                },
+            },
+            'theme': {
+                'label': 'Theme',
+                'type': 'select',
+                'options': {
+                    "blazer": "blazer",
+                    "chalkboard": "chalkboard",
+                    "dark_pastel": "dark_pastel",
+                    "desert": "desert",
+                    "espresso": "espresso",
+                    "github": "github",
+                    "grass": "grass",
+                    "homebrew": "homebrew",
+                    "hurtado": "hurtado",
+                    "idletoes": "idletoes",
+                    "kibble": "kibble",
+                    "man_page": "man_page",
+                    "monokai_soda": "monokai_soda",
+                    "neopolitan": "neopolitan",
+                    "novel": "novel",
+                    "ocean": "ocean",
+                    "pro": "pro",
+                    "red_sands": "red_sands",
+                    "seafoam_pastel": "seafoam_pastel",
+                    "solarized_darcula": "solarized_darcula",
+                    "solarized_dark": "solarized_dark",
+                    "solarized_light": "solarized_light",
+                    "symfonic": "symfonic",
+                    "terminal_basic": "terminal_basic",
+                    "vaughn": "vaughn",
+                    "zenburn": "zenburn",
                 }
             }
         }
