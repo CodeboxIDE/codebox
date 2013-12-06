3b2e100dc8e32b4bb7660dd94330539925dd8a61
Aaron O'Mullan <aaron.omullan@friendco.de>
2013-12-05 23:29:10 -0800

    Filter out codebox's current port from cb.proc/http listing

--- src/core/cb.proc/main.js
+++ src/core/cb.proc/main.js
@@ -4,12 +4,25 @@ var _ = require('underscore');
 var http = require('./http').list;
 
 function setup(options, imports, register) {
+    var serverPort = imports.server.port;
 
+    // Function to filter out Codebox's current port
+    var portFilter = function portFilter(res) {
+        return res.port != serverPort;
+    };
+
+    // Listing function adapted for current running instance
+    var _http = function _http() {
+        return http(options.urlPattern)
+        .then(function(results) {
+            return results.filter(portFilter);
+        });
+    };
 
     register(null, {
         'proc': {
             // List using the given urlPattern
-            'http': _.partial(http, options.urlPattern)
+            'http': _http
         }
     });
 }
