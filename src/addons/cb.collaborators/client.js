d01092a29058fb63c09dcf12b00cc804381ebb54
Samy Pessé <samypesse@gmail.com>
2013-12-18 01:59:05 +0100

    Remove collaborators panels and prepare menu

---
+++ src/addons/cb.collaborators/client.js
@@ -0,0 +1,10 @@
+define([], function() {
+    var $ = codebox.require("jQuery");
+    var collaborators = codebox.require("core/collaborators");
+    var search = codebox.require("core/search");
+    var box = codebox.require("core/box");
+    var user = codebox.require("core/user");
+    var commands = codebox.require("core/commands");
+
+    
+});
