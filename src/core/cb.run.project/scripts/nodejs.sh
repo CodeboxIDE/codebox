15b89d3dc0ebd86b63af96c8c5ab9bc56fe7bc49
Aaron O'Mullan <aaron.omullan@friendco.de>
2014-01-02 17:05:42 +0100

    Switch nodejs.sh running script to use npm start

--- src/core/cb.run.project/scripts/nodejs.sh
+++ src/core/cb.run.project/scripts/nodejs.sh
@@ -3,19 +3,5 @@
 WORKSPACE=$1
 PORT=$2
 
-function json_value {
-    # JSON data
-    local json = $1
-
-    # Property to extract
-    local prop = $2
-
-    # Extract value
-    local temp = echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $prop| cut -d":" -f2| sed -e 's/^ *//g' -e 's/ *$//g'
-
-    # Return value
-    echo ${temp##*|}
-}
-
-# Data
-JSON=$(cat ${})
+# Try running using "npm start convention"
+npm start
