514ad65e41e35cb9b58391c2301519c7b1b702fa
Aaron O'Mullan <aaron.omullan@friendco.de>
2014-01-02 17:32:03 +0100

    Hookup shells to eventbus

--- src/core/cb.shells/main.js
+++ src/core/cb.shells/main.js
@@ -3,7 +3,9 @@ var _ = require('underscore');
 
 var shux = require('shux');
 
+
 function setup(options, imports, register) {
+    var events = imports.events;
     var workspace = imports.workspace;
 
     // Construct
@@ -16,6 +18,22 @@ function setup(options, imports, register) {
             cwd: workspace.root
         }));
     };
+
+
+    // Utility function for connecting emitter to eventbus
+    // (converts arguments to data convention)
+    var emit = function shellEmit(events, eventId, shellId) {
+        return events.emit(eventId, {
+            shellId: shellId
+        });
+    };
+
+    // Connect shell manger to event bus
+    manager.on('exit', _.partial(emit, 'shell.exit'));
+    manager.on('spawn', _.partial(emit, 'shell.spawn'));
+
+    manager.on('attach', _.partial(emit, 'shell.attach'));
+    manager.on('detach', _.partial(emit, 'shell.detach'));
 
     // Register
     register(null, {
