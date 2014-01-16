30c2f5f2b8f9f1968bab4b7894b349191276582f
Aaron O'Mullan <aaron.omullan@friendco.de>
2014-01-16 21:38:12 +0100

    Add hhvm support to php project runner

--- core/cb.run.project/scripts/php.sh
+++ core/cb.run.project/scripts/php.sh
@@ -3,4 +3,10 @@
 WORKSPACE=$1
 PORT=$2
 
-php -S "0.0.0.0:${PORT}" -t ${WORKSPACE}
+
+# Use HHVM if it exists
+if [[ ! -z "$(which hhvm)" ]]; then
+    bash -c "cd ${WORKSPACE} && hhvm -m server -p ${PORT}"
+else
+    php -S "0.0.0.0:${PORT}" -t ${WORKSPACE}
+fi
