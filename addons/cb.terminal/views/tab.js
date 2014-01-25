71c551ddead2739fdff91dacc8a29b61d8a064bc
Samy Pessé <samypesse@gmail.com>
2014-01-25 12:33:17 +0100

    Limit size of terminal random id

--- addons/cb.terminal/views/tab.js
+++ addons/cb.terminal/views/tab.js
@@ -77,7 +77,7 @@ define([
             // Init codebox stream
             this.sessionId = this.options.shellId || _.uniqueId("term");
             this.shell = box.openShell({
-                'shellId': this.options.shellId ? this.sessionId : this.sessionId+"-"+(new Date()).getTime()
+                'shellId': this.options.shellId ? this.sessionId : this.sessionId+"-"+(new Date()).getSeconds()
             });
 
             this.on("tab:close", function() {
