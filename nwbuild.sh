6871dc0be9f1f344487033dd3db32f74badeb509
Aaron O'Mullan <aaron.omullan@friendco.de>
2014-01-29 21:50:49 +0100

    Fix nwbuild.sh multi module support
    It wasn’t iterating the array correctly

--- nwbuild.sh
+++ nwbuild.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 NW_VERSION=${NW_VERSION:=0.8.0}
-REBUILD_MODULES=("shux/node_modules/pty.js", "ctags")
+REBUILD_MODULES=("shux/node_modules/pty.js" "ctags")
 
 function rebuild_module () {
   bash -c "cd $1 && nw-gyp rebuild --target=$NW_VERSION"
 }
 
-for module in $REBUILD_MODULES; do
+for module in ${REBUILD_MODULES[@]}; do
   rebuild_module node_modules/$module
 done
