5aa8a92f53ff9a79596a0479fa78930039483837
Aaron O'Mullan <aaron.omullan@friendco.de>
2014-02-01 04:06:53 +0100

    Fix: executing _run_apache.sh from php/run.sh

--- core/cb.project/php/run.sh
+++ core/cb.project/php/run.sh
@@ -3,13 +3,15 @@
 WORKSPACE=$1
 PORT=$2
 
+# Dir of current script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Use HHVM if it exists
 if [[ ! -z "$(which hhvm)" ]]; then
     bash -c "cd ${WORKSPACE} && hhvm -m server -p ${PORT}"
 # Use Apache2 if it is installed
 elif [[ ! -z "$(which apachectl)" ]]; then
-    exec ./_run_apache.sh ${WORKSPACE} ${PORT}
+    exec "${DIR}/_run_apache.sh" ${WORKSPACE} ${PORT}
 # Fallback to builtin PHP server
 else
     php -S "0.0.0.0:${PORT}" -t ${WORKSPACE}
