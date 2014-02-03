f221a0a284b596cafa2608decc815f0ce2a2079a
Aaron O'Mullan <aaron.omullan@friendco.de>
2014-02-03 19:39:12 +0100

    Remove some unused languages from our ace build

--- addons/cb.files.editor/build.sh
+++ addons/cb.files.editor/build.sh
@@ -3,6 +3,7 @@
 # This script will prepare ace for running properly
 
 RM_EXTENSIONS=(elastic_tabstops_lite chromevox statusbar emmet error_marker keybinding_menu old_ie textarea themelist static_highlight split spellcheck settings_menu)
+RM_LANG=(jsoniq xquery abap cobol forth mushcode vbscript tcl velocity pascal)
 
 # Remove useless extensions
 for ext in ${RM_EXTENSIONS[*]}
@@ -10,6 +11,14 @@ do
     echo "Remove extension $ext"
     rm -f ace/ext-$ext.js
 done
+
+# Remove languages
+for lang in ${RM_LANG[*]}
+do
+    echo "Remove language $lang"
+    rm -f "ace/mode-$lang.js" "ace/snippets/$lang.js" "ace/worker-$lang.js"
+done
+
 
 # Remove useless themes
 echo "Removes themes"
