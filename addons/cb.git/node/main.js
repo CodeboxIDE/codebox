fc9a1f44497d59511a0bf7963666f52bbbf5f7eb
Aaron O'Mullan <aaron.omullan@friendco.de>
2014-02-05 22:29:15 +0100

    Move git support from core/ to addons/cb.git

--- addons/cb.git/node/main.js
+++ addons/cb.git/node/main.js
@@ -3,19 +3,30 @@ var _ = require('underscore');
 
 var Gittle = require('gittle');
 
+var GitRPCService = require('./service').GitRPCService;
+
 
 function setup(options, imports, register) {
     // Import
+    var rpc = imports.rpc;
+    var events = imports.events;
     var workspace = imports.workspace;
 
     // Construct
     var repo = new Gittle(workspace.root);
 
+    // Service
+    var service = new GitRPCService(repo, events);
+
+    // Register RPC
+    rpc.register('git', service);
+
     // Register
     register(null, {
         "git": {
             repo: repo,
-        }
+        },
+        "git_rpc": service
     });
 }
 
