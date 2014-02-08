241ccc899524f18b6d779081e12e049e264657b7
Samy Pessé <samypesse@gmail.com>
2014-02-08 12:12:07 +0100

    Add base node.js sample

--- core/cb.project/nodejs/index.js
+++ core/cb.project/nodejs/index.js
@@ -4,6 +4,7 @@ module.exports = {
     id: "nodejs",
     name: "Node.JS",
 
+    sample: path.resolve(__dirname, "sample"),
     detector: path.resolve(__dirname, "detector.sh"),
     runner: [
         {
