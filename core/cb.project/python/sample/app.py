839bd92b360e1d90eec62a84e2a5e836698d041a
Aaron O'Mullan <aaron.omullan@friendco.de>
2014-02-09 00:05:23 +0100

    Make port dynamic in flask sample

--- core/cb.project/python/sample/app.py
+++ core/cb.project/python/sample/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 app = Flask(__name__)
 
@@ -6,4 +8,6 @@ def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(
+        port=int(os.environ.get('PORT', 5000))
+    )
