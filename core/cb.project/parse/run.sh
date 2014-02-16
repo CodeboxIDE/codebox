9a3225a44f89ec0c4e6c4f80cc0067235a5267cc
Samy Pessé <samypesse@gmail.com>
2014-02-17 00:21:07 +0100

    Fix runner for parse

--- core/cb.project/parse/run.sh
+++ core/cb.project/parse/run.sh
@@ -4,4 +4,9 @@
 WORKSPACE=$1
 PORT=$2
 
-cd $WORKSPACE && parse develop
+# Get app name
+APPNAME=`cat ${WORKSPACE}/config/global.json | python -c "import json; import sys; print json.load(sys.stdin)['applications']['_default']['link']"`
+
+echo "Start Parse application '${APPNAME}' (read from config/global.json)"
+
+cd $WORKSPACE && parse develop $APPNAME
