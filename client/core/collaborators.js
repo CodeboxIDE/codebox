413d998e9a23cf6dea7653a87fef26e52773a622
Samy Pessé <samypesse@gmail.com>
2014-02-20 01:12:52 +0100

    Add alert message when users joined and left the workspace

--- client/core/collaborators.js
+++ client/core/collaborators.js
@@ -1,10 +1,19 @@
 define([
     'hr/utils',
     'hr/hr',
-    'collections/users'
-], function (_, hr, Users) {
+    'collections/users',
+    'utils/alerts'
+], function (_, hr, Users, alerts) {
     // Collection for all current collaborators
     var collaborators = new Users();
+
+    collaborators.on("add", function(user) {
+        alerts.show(user.get("name")+" just joined the workspace", 5000);
+    });
+
+    collaborators.on("remove", function(user) {
+        alerts.show(user.get("name")+" just left the workspace", 5000);
+    });
 
     return collaborators;
 });
