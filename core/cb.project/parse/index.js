1012fb705859f61eb097f34a258050b837a593df
Samy Pessé <samypesse@gmail.com>
2014-02-21 16:29:35 +0100

    Add id of mobile stack to parse sample

--- core/cb.project/parse/index.js
+++ core/cb.project/parse/index.js
@@ -3,6 +3,7 @@ var path = require("path");
 module.exports = {
     id: "parse",
     name: "Parse",
+    otherIds: ["mobile"],
 
     sample: path.resolve(__dirname, "sample"),
     detector: path.resolve(__dirname, "detector.sh"),
