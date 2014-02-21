5ae0fff2a1d4916320c76012e15ddf8ce2a5d398
Samy Pessé <samypesse@gmail.com>
2014-02-21 01:29:08 +0100

    Use nw-gyp from node_modules

--- scripts/nwbuild.sh
+++ scripts/nwbuild.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+SCRIPTPATH=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+
 NW_VERSION=${NW_VERSION:=0.8.0}
 REBUILD_MODULES=("shux/node_modules/pty.js" "ctags" "gittle/node_modules/pty.js" "vfs-local/node_modules/pty.js")
 
 function rebuild_module () {
-  bash -c "cd $1 && nw-gyp rebuild --target=$NW_VERSION"
+  bash -c "cd $1 && $SCRIPTPATH/../node_modules/.bin/nw-gyp rebuild --target=$NW_VERSION"
 }
 
 for module in ${REBUILD_MODULES[@]}; do
