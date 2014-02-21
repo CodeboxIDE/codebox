ef27ef092bac005ba7eba60600067d2e9e4fa4d1
Samy Pessé <samypesse@gmail.com>
2014-02-21 18:07:33 +0100

    Fix linux build script

--- scripts/build_linux_tar.sh
+++ scripts/build_linux_tar.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SOURCE=appBuilds/releases/Codebox/linux32/Codebox
+SOURCE=appBuilds/releases/Codebox/linux32/
 OUTPUT=appBuilds/releases/codebox-linux32.tar.gz
 
 echo "Building Linux Tar: $OUTPUT"
-tar -zcvf $OUTPUT ${SOURCE}
+tar -zcvf $OUTPUT -C ${SOURCE} Codebox
