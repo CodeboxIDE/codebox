75dd75b5453a2b8f2b4ca46fea42607ec0d426f2
Samy Pessé <samypesse@gmail.com>
2014-02-21 01:59:49 +0100

    Fix app building for mac and linux

--- scripts/nwbuild.sh
+++ scripts/nwbuild.sh
@@ -2,11 +2,12 @@
 
 SCRIPTPATH=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
-NW_VERSION=${NW_VERSION:=0.8.0}
+NW_VERSION=$1
 REBUILD_MODULES=("shux/node_modules/pty.js" "ctags" "gittle/node_modules/pty.js" "vfs-local/node_modules/pty.js")
 
+echo $NW_VERSION
 function rebuild_module () {
-  bash -c "cd $1 && $SCRIPTPATH/../node_modules/.bin/nw-gyp rebuild --target=$NW_VERSION"
+  bash -c "cd $1 && $SCRIPTPATH/../node_modules/nw-gyp/bin/nw-gyp.js rebuild --target=$NW_VERSION"
 }
 
 for module in ${REBUILD_MODULES[@]}; do
