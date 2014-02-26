ac8ffdcd4f775427da3a06f044d9110c9ea1be49
Samy Pessé <samypesse@gmail.com>
2014-02-26 23:40:17 +0100

    Add base resize for sections

--- client/views/tabs/grid.js
+++ client/views/tabs/grid.js
@@ -50,20 +50,32 @@ define([
         },
 
         /*
+         *  Return current layout
+         */
+        getLayout: function() {
+            var layout = this.columns || Math.floor(Math.sqrt(this.views.length));
+
+            var nColumns = Math.min(layout, this.views.length);
+            var nLines = Math.ceil(this.views.length/layout);
+
+            return {
+                'columns': nColumns,
+                'lines': nLines
+            };
+        },
+
+        /*
          *  Re-render the complete layout
          */
         render: function() {
             var x, y;
             this.$el.empty();
 
-            // Calcul layout if auto
-            var layout = this.columns || Math.floor(Math.sqrt(this.views.length));
+            // Calcul layout
+            var layout = this.getLayout();
 
-            var nColumns = Math.min(layout, this.views.length);
-            var nLines = Math.ceil(this.views.length/layout);
-
-            var sectionWidth = (100/nColumns).toFixed(3);
-            var sectionHeight = (100/nLines).toFixed(3);
+            var sectionWidth = (100/layout.columns).toFixed(3);
+            var sectionHeight = (100/layout.lines).toFixed(3);
 
             // Add grid content
             x = 0; y = 0;
@@ -83,7 +95,7 @@ define([
                 section.appendTo(this.$el);
 
                 // Resize bar
-                if (x < (nColumns - 1)) {
+                if (x < (layout.columns - 1)) {
                     // Horrizontal
                     var hBar = $("<div>", {
                         'class': "grid-resize-bar-h",
@@ -92,7 +104,7 @@ define([
                     hBar.appendTo(section);
                 }
 
-                if (y < (nLines - 1)) {
+                if (y < (layout.lines - 1)) {
                     // Vertical
                     var vBar = $("<div>", {
                         'class': "grid-resize-bar-v",
@@ -102,9 +114,9 @@ define([
                 }
 
 
-                 // Calcul next position
+                // Calcul next position
                 x = x + 1;
-                if (x >= nColumns) {
+                if (x >= layout.columns) {
                     x = 0;
                     y = y + 1;
                 }
@@ -115,6 +127,7 @@ define([
 
         // Create a resizer handler
         resizerHandler: function(x, y, type) {
+            var that = this;
             var $document = $(document);
             var oX, oY, dX, dY;
             return function(e) {
@@ -128,9 +141,14 @@ define([
 
                     if (type == "h") {
                         console.log("move h", dx);
+                        that.resizeColumn(y, -dx);
                     } else {
                         console.log("move v", dy);
+                        that.resizeLine(x, -dy);
                     }
+
+                    oX = e.pageX;
+                    oY = e.pageY;
                 };
 
                 $document.mousemove(f);
@@ -138,6 +156,43 @@ define([
                     $document.unbind('mousemove', f);
                 });
             };
+        },
+
+        getSection: function(sx, sy) {
+            var x, y, layout = this.getLayout(), that = this;
+
+            x = 0; y = 0;
+            return this.$("> .grid-section").filter(function() {
+                var r = false;
+
+                if ((sx != null && sx == x)
+                || (sy != null && sy == y)) {
+                    r = true;
+                }
+
+                // Calcul next position
+                x = x + 1;
+                if (x >= layout.columns) {
+                    x = 0;
+                    y = y + 1;
+                }
+
+                return r;
+            });
+        },
+
+        resizeLine: function(i, d) {
+            var $sections = this.getSection(null, i);
+            var h = $sections.height();
+
+            $sections.height(h+d);
+        },
+
+        resizeColumn: function(i, d) {
+            var $sections = this.getSection(i, null);
+            var w = $sections.width();
+
+            $sections.width(w+d);
         }
     });
 
