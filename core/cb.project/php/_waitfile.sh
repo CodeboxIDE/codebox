6c05aaf41b70b035b305b1021f16be653f9efd51
Aaron O'Mullan <aaron.omullan@friendco.de>
2014-03-04 18:27:28 +0100

    add php/_waitfile.sh script
    Waits for till a file exists and fails after a certain timeout

---
+++ core/cb.project/php/_waitfile.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+filename=$1
+timeout=$2
+
+CMD="
+while [ ! -f ${filename} ]
+do
+    echo tick
+    sleep 0.2
+done
+"
+
+# Return when file exists with timeout
+bash -c "${CMD}" & \
+sleep ${timeout};
+
+# Try killning bash shell
+kill $! &> /dev/null
+RES=$?
+
+# If killing succeeds then fail,
+# succeed if killing fails (process already dead)
+if [ ${RES} = 0 ]; then
+    exit 1
+else
+    exit 0
+fi
