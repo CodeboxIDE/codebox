31e1a74e230cba400f4d53b4141bbf01431f997f
Aaron O'Mullan <aaron.omullan@friendco.de>
2014-03-22 19:17:05 -0700

    Fix Simple java project runner

--- core/cb.project/java/run.sh
+++ core/cb.project/java/run.sh
@@ -14,11 +14,19 @@ WORKSPACE=$1
 PORT=$2
 
 # Get plausible entry point
-entry_point=$(${FGREP} " main(" -R ${WORKSPACE} | tr ':' '\n' | grep ".java" | head -n 1)
+entry_point=$(${FGREP} " public static void main" -R ${WORKSPACE} | tr ':' '\n' | grep ".java" | head -n 1)
+clean_point=${entry_point%.*}
+class_point="${clean_point}.class"
 
 if [ -f ${entry_point} ]; then
     echo "Compiling ${entry_point}"
-    exec javac ${entry_point}
+    javac ${entry_point}
+
+    echo "Running ${class_point}"
+    echo ""
+    java $(basename ${clean_point})
+
+    rm -f "${class_point}"
 else
     # Exit
     exit 1
