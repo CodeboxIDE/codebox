936de16970bdd17269a621c9265c754d22fbccd8
Samy Pessé <samypesse@gmail.com>
2014-03-28 23:07:43 -0700

    Add documentation for utils/css

--- client/utils/css.js
+++ client/utils/css.js
@@ -1,51 +1,62 @@
-define([], function() {
+define([
+    "hr/utils"
+], function(_) {
+    /**
+     * Utils for managing css and styling
+     *
+     * @class
+     */
+    var css = {
+        /**
+         * Convert a json formatted data to some css
+         *
+         * @param {object} css css json-formatted
+         * @param {object} options options for convertion
+         */
+        convertJSON: function(css, options) {
+            var nodes = [];
+            options = _.defaults(options || {}, {
+                'base': "",
+                'namespace': {},
+                'joint': "\n"
+            });
 
-    // Convert json to css
-    var convertFromJSON = function(css, options) {
-        var nodes = [];
-        options = _.defaults(options || {}, {
-            'base': "",
-            'namespace': {},
-            'joint': "\n"
-        });
+            var convertNode = function(node, base) {
+                return _.map(node, function(value, property) {
+                    var result = "", children = "", selector, attr= "";
 
-        var convertNode = function(node, base) {
-            return _.map(node, function(value, property) {
-                var result = "", children = "", selector, attr= "";
+                    // Property
+                    if (_.isString(value)) {
+                        return property + ": " + value + ";"
+                    }
 
-                // Property
-                if (_.isString(value)) {
-                    return property + ": " + value + ";"
-                }
+                    // Sub element
+                    if (property[0] == '&') {
+                        selector = base;
+                        attr = property;
+                    } else {
+                        selector = base ? base+" "+property : property;
+                    }
+                    nodes.push({
+                        'selector': selector,
+                        'attr': attr,
+                        'content': convertNode(value, selector)
+                    });
 
-                // Sub element
-                if (property[0] == '&') {
-                    selector = base;
-                    attr = property;
-                } else {
-                    selector = base ? base+" "+property : property;
-                }
-                nodes.push({
-                    'selector': selector,
-                    'attr': attr,
-                    'content': convertNode(value, selector)
-                });
+                    return "";
+                }).join(options.joint);
+            };
 
-                return "";
+            convertNode(css);
+
+            // Convert nodes list to text
+            return _.map(nodes, function(node) {
+                var selector = options.base + " " + (options.namespace[node.selector] || node.selector);
+                selector = selector + node.attr.replace("&", "");
+                return selector+" {"+options.joint+node.content+options.joint+"}";
             }).join(options.joint);
-        };
+        }
+    }
 
-        convertNode(css);
-
-        // Convert nodes list to text
-        return _.map(nodes, function(node) {
-            var selector = options.base + " " + (options.namespace[node.selector] || node.selector);
-            selector = selector + node.attr.replace("&", "");
-            return selector+" {"+options.joint+node.content+options.joint+"}";
-        }).join(options.joint);
-    };
-
-    return {
-        'convertJSON': convertFromJSON
-    };
+    return _.bindAll(css);
 });
