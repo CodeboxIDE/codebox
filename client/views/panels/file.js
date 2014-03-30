78d33d902aebaa1c137f04fa61a0b8458c186eb1
Samy Pessé <samypesse@gmail.com>
2014-03-29 18:26:25 -0700

    Update file panels when active files change

--- client/views/panels/file.js
+++ client/views/panels/file.js
@@ -30,9 +30,18 @@ define([
             PanelFileView.__super__.initialize.apply(this, arguments);
 
             var box = codebox.require("core/box");
+            var files = codebox.require("core/files");
 
-            // Update tags when file changes
+            // Remove view when close file
+            this.listenTo(files.active, "remove", this.detachFile);
+
+            // Update view when file changes
             this.listenTo(box, "file.active", this.update);
+
+            // Update file view when changes
+            this.listenTo(box, "box:watch:change", function(e) {
+                this.updateFile(e.data.path);
+            });
 
             // Different cached file views
             this.fileViews = {};
@@ -63,6 +72,31 @@ define([
             this.fileViews[path].$el.appendTo(this.$el);
 
             return this.ready();
+        },
+
+        /**
+         * Detach a file from this file panel
+         *
+         * @private
+         */
+        detachFile: function(file) {
+            var path = _.isString(file) ? file : file.path();
+            if (!this.fileViews[path]) return;
+
+            this.fileViews[path].remove();
+            delete this.fileViews[path];
+        },
+
+        /**
+         * Update a file
+         *
+         * @private
+         */
+        updateFile: function(file) {
+            var path = _.isString(file) ? file : file.path();
+            if (!this.fileViews[path]) return;
+            
+            this.fileViews[path].update();
         }
     });
 
