a1c0ed8e686c8ab56719b60abe85250f213700cf
Robin Dietrich <me@invokr.org>
2014-05-14 10:30:19 +0200

    Added automatic refreshing of open preview tabs on file save

--- addons/cb.files.preview/views/preview.js
+++ addons/cb.files.preview/views/preview.js
@@ -4,15 +4,45 @@ define([
 ], function(templateFile) {
     var _ = codebox.require("hr/utils");
     var $ = codebox.require("hr/dom");
-    var hr = codebox.require("hr/hr");
-    var Dialogs = codebox.require("utils/dialogs");
-    var FilesBaseView = codebox.require("views/files/base");
+    var box = codebox.require("core/box");
+    var FilesTabView = codebox.require("views/files/tab");
 
-    var PreviewView = FilesBaseView.extend({
+    var PreviewView = FilesTabView.extend({
         className: "addon-files-previewviewer",
         templateLoader: "text",
         template: templateFile,
-        events: {}
+        events: {},
+        
+        initialize: function() {
+            PreviewView.__super__.initialize.apply(this, arguments);
+            var that = this;
+            
+            // add refresh menu option
+            this.tab.menu.menuSection([
+                {
+                    'type': "action",
+                    'title': "Refresh",
+                    'shortcuts': [
+                        "mod+r"
+                    ],
+                    'bindKeyboard': true,
+                    'action': function() {
+                        that.refresh();
+                    }
+                }
+            ]);
+            
+            // bind save event 
+            box.on("box:watch:change:update", function() {
+                that.refresh();
+            }, this);
+            
+            return this;
+        },
+        
+        refresh: function() {
+            $(this.$el).find("iframe").attr('src', function ( i, val ) { return val; });
+        }
     });
 
     return PreviewView;
