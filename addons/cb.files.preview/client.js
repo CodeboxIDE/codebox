6fead0768f360d21e29ac93100f71b44e513359e
Robin Dietrich <me@invokr.org>
2014-05-15 13:09:18 +0200

    Added markdown preview handler

--- addons/cb.files.preview/client.js
+++ addons/cb.files.preview/client.js
@@ -1,6 +1,7 @@
 define([
-    "views/preview_html"
-], function(PreviewHtml) {
+    "views/preview_html",
+    "views/preview_markdown"
+], function(PreviewHtml, PreviewMarkdown) {
     var _ = codebox.require("hr/utils");
     var files = codebox.require("core/files");
 
@@ -15,6 +16,20 @@ define([
         View: PreviewHtml,
         valid: function(file) {
             return (!file.isDirectory() && _.contains(htmlExts, file.extension()));
+        }
+    });
+    
+    var markdownExts = [
+        ".md", ".markdown", ".txt"
+    ];
+    
+    files.addHandler("preview-markdown", {
+        name: "Preview",
+        icon: "eye",
+        position: 10,
+        View: PreviewMarkdown,
+        valid: function(file) {
+            return (!file.isDirectory() && _.contains(markdownExts, file.extension()));
         }
     });
 });
