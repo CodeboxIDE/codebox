c320905c65086ee9b989b3fd4e5ec55492141eb1
Samy Pessé <samypesse@gmail.com>
2014-07-15 21:40:04 -0700

    Add workspace concept and root directory

--- bin/codebox.js
+++ bin/codebox.js
@@ -1,9 +1,11 @@
 #! /usr/bin/env node
 
+var path = require("path");
 var codebox = require("../lib");
 
 codebox.start({
-    port: 3000
+    port: 3000,
+    root: path.resolve(__dirname, "../")
 })
 .fail(function(err) {
     console.log(err.stack || err.message || err);
