cd250893765f3bb2748f5740c45c954475733a85
Samy Pessé <samypesse@gmail.com>
2014-07-20 15:05:15 -0700

    Add base command line interface

--- bin/codebox.js
+++ bin/codebox.js
@@ -1,11 +1,26 @@
 #! /usr/bin/env node
 
 var path = require("path");
+var program = require('commander');
+
+var pkg = require("../package.json");
 var codebox = require("../lib");
 
-codebox.start({
-    root: path.resolve(__dirname, "../../test")
-})
+program
+.version(pkg.version)
+.option('-r, --root [path]', 'Root folder for the workspace, default is current directory', "./")
+.option('-t, --templates [list]', 'Configuration templates, separated by commas', "")
+.option('-p, --port [port]', 'HTTP port', 3000)
+.parse(process.argv);
+
+// Generate configration
+var options = {
+    root: path.resolve(process.cwd(), program.root),
+    port: program.port
+};
+
+
+codebox.start(options)
 .fail(function(err) {
     console.log(err.stack || err.message || err);
 });
