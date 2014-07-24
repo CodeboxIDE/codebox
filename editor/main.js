dbb3ecb596e6b591167c562446bdef3c0c20d8f3
Samy Pessé <samypesse@gmail.com>
2014-07-24 14:39:25 -0700

    Add base for context menu

--- editor/main.js
+++ editor/main.js
@@ -8,8 +8,9 @@ require([
     "core/application",
     "core/packages",
     "utils/dialogs",
+    "utils/menu",
     "models/file"
-], function(_, $, Q, hr, args, resources, app, packages, dialogs, File) {
+], function(_, $, Q, hr, args, resources, app, packages, dialogs, menu, File) {
     // Create the global object for packages
     window.codebox = {
         require: require,
