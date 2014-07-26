39256a5c590cb125f3c16e157f4b138de69a750d
Samy Pessé <samypesse@gmail.com>
2014-07-25 23:47:22 -0700

    Update list of users when event received

--- editor/core/users.js
+++ editor/core/users.js
@@ -1,7 +1,13 @@
 define([
-    "collections/users"
-], function(Users) {
+    "collections/users",
+    "core/events"
+], function(Users, events) {
     var users = new Users();
+
+    // Listen to update
+    events.on("e:users", function() {
+        users.listAll();
+    });
 
     return users;
 });
