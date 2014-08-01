b55c5a705db23a08cfd573d07f0ecf77107894d3
Samy Pessé <samypesse@gmail.com>
2014-08-01 16:52:32 -0700

    Improve base getting for form view

--- editor/views/form.js
+++ editor/views/form.js
@@ -1,8 +1,75 @@
 define([
     "hr/utils",
     "hr/dom",
-    "hr/hr"
-], function(_, $, hr) {
+    "hr/hr",
+    "text!resources/templates/form.html"
+], function(_, $, hr, formTemplate) {
+
+    var GETTER = {
+        'boolean': function() {
+            return $(this).is(":checked");
+        },
+        'string': function() {
+            return $(this).val();
+        },
+        'number': function() {
+            return parseInt($(this).val());
+        }
+    };
+
+    var RENDERER = {
+        'boolean': function(propertyName, property, value) {
+            return $("<input>", {
+                'type': "checkbox",
+                'checked': value === true,
+                'name': propertyName
+            });
+        },
+
+        'number': function(propertyName, property, value) {
+            return $("<input>", {
+                'type': "number",
+                'value': value,
+                'name': propertyName,
+                'min': property.minimum,
+                'max': property.maximum,
+                'step': property.multipleOf || 1
+            });
+        },
+
+        'string': function(propertyName, property, value) {
+            if (property['enum']) return RENDERER.select.apply(this, arguments);
+
+            return $("<input>", {
+                'type': "text",
+                'value': value,
+                'name': propertyName
+            });
+        },
+
+        'select': function(propertyName, property, value) {
+            var $select = $("<select>", {
+                'name': propertyName
+            });
+
+            $select.append(_.map(property['enum'], function(v) {
+                return $("<option>", {
+                    'selected': value == v,
+                    'text': v,
+                    'value': v
+                });
+            }));
+
+            return $select;
+        },
+
+        'array': function() {
+            return $("<button>", {
+                "class": "button",
+                "text": "Add"
+            });
+        }
+    };
 
     var SchemaView = hr.View.extend({
         className: "form-schema",
@@ -19,8 +86,10 @@ define([
 
         render: function() {
             var values = this.options.values;
+            this.$el.empty();
 
             _.each(this.options.schema.properties, function(property, propertyName) {
+                var value = values[propertyName];
                 var $property = $("<div>", {
                     'class': "schema-property type-"+property.type
                 });
@@ -33,11 +102,21 @@ define([
                 if (property.type == "object") {
                     var schema = new SchemaView({
                         'schema': property,
-                        'values': values[propertyName] || {}
+                        'values': value || {},
+                        'prefix': propertyName
                     });
                     schema.$el.appendTo($property);
                     schema.update();
+                } else {
+                    var renderer = RENDERER[property.type];
+                    var $input = renderer(_.compact([this.options.prefix, propertyName]).join("."), property, value);
+                    $input.attr("data-getter", property.type);
+
+                    $input.appendTo($property);
+
                 }
+
+
 
                 $property.appendTo(this.$el);
             }, this);
@@ -51,9 +130,13 @@ define([
         className: "component-form",
         defaults: {
             schema: {},
-            values: {}
+            values: {},
+            submit: "Submit"
         },
-        events: {},
+        events: {
+            "click .form-submit button": "onSubmit"
+        },
+        template: formTemplate,
 
         initialize: function() {
             FormView.__super__.initialize.apply(this, arguments);
@@ -64,7 +147,45 @@ define([
                 values: this.options.values
             });
             this.schema.update();
-            this.schema.appendTo(this);
+        },
+
+        templateContext: function() {
+            return {
+                options: this.options
+            };
+        },
+
+        render: function() {
+            this.schema.detach();
+            this.schema.update();
+
+            return FormView.__super__.render.apply(this, arguments);
+        },
+
+        finish: function() {
+            this.schema.$el.appendTo(this.$(".form-content"));
+
+            return FormView.__super__.finish.apply(this, arguments);
+        },
+
+        // Extract all new values
+        getValues: function() {
+            var m = new hr.Model({}, this.options.values);
+            this.$("*[name]").each(function() {
+                var name = $(this).attr("name");
+                var getter = GETTER[$(this).data("getter")];
+                var value = getter.apply($(this));
+
+                console.log("get", name, value);
+                m.set(name, value);
+            });
+
+            return m.toJSON();
+        },
+
+        // On submit
+        onSubmit: function(e) {
+            this.trigger("submit", this.getValues());
         }
     });
 
