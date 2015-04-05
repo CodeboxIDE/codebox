162e240d431e01865c1db3b87e27265575c2e91f
Samy Pessé <samypesse@gmail.com>
2015-04-06 00:20:43 +0200

    Add base gulpfile and hr dependencies

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp');
+var del = require('del');
+var less = require('gulp-less');
+var browserify = require('gulp-browserify');
+var runSequence = require('gulp-run-sequence');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var stringify = require('stringify');
+
+// Compile Javascript
+gulp.task('scripts', function() {
+    return gulp.src('editor/main.js')
+    .pipe(browserify({
+        debug: false,
+        transform: ['stringify']
+    }))
+    //.pipe(uglify())
+    .pipe(rename('application.js'))
+    .pipe(gulp.dest('./build/static/js'));
+});
+
+// Copy html
+gulp.task('html', function() {
+    return gulp.src('editor/index.html')
+    .pipe(rename('index.html'))
+    .pipe(gulp.dest('./build/static/'));
+});
+
+// Less to css
+gulp.task('styles', function() {
+    return gulp.src('./editor/resources/stylesheets/main.less')
+    .pipe(less())
+    .pipe(rename('application.css'))
+    .pipe(gulp.dest('./build/static/css'));
+});
+
+// Clean output
+gulp.task('clean', function(cb) {
+    del(['dist/**'], cb);
+});
+
+gulp.task('default', function(cb) {
+    runSequence('clean', ['scripts', 'styles', 'html'], cb);
+});
